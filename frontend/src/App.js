938d9dacab1262de85c9bf3d1fb4bc5117b229bc
unknown <shashankaggarwal13@gmail.com>
2026-01-13 23:24:52 +0530

    Edit logic updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const API_URL = 'https://b2fd0c23ca0e.ngrok-free.app'; 
+const API_URL = 'https://29a2c4aefbea.ngrok-free.app'; 
 
 function App() {
   const [payers, setPayers] = useState([]);
